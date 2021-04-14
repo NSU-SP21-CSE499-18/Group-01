cf97bb1cda253ecf56912ebbfc420b5cba03a75e
ferdouszislam <ferdouszislam@gmail.com>
2021-04-15 02:58:20 +0600

    Practice #4: Kotlin practice with arraylist, function, conditional

--- Exercise/kotlin_practice/ferdous/Main.kt
+++ Exercise/kotlin_practice/ferdous/Main.kt
@@ -1,5 +1,34 @@
 
+fun sumTwoNumbers(a:Int, b:Int) : String?{
+
+    return "$a + $b = " + (a+b)
+}
+
+fun arrayListPractice(){
+
+    var numbers = arrayListOf(2, 1, 5, 6, 3, 4)
+
+    numbers.sort()
+
+    var lastTwoNumbers = arrayListOf<Int>()
+
+    numbers.forEach {
+
+        lastTwoNumbers.add(it)
+
+        when(lastTwoNumbers.size){
+
+            2 -> {
+
+                println(sumTwoNumbers(lastTwoNumbers[0], lastTwoNumbers[1]))
+
+                lastTwoNumbers.removeAt(0)
+            }
+        }
+    }
+}
+
 fun main() {
 
-    println("Hello Kotlin!")
+    arrayListPractice()
 }

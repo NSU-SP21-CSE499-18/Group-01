fbdaadeeeb07a92fa6d25a02df8c2d41fff2a2ee
Rifat Islam <rifat.islam173@northsouth.edu>
2021-04-21 16:24:59 +0600

    Practice #4: Done with Code Lab 01 of Chapter 01

---
+++ Exercise/rifat/practice/unit-1/app/src/test/java/com/ece/nsu/spring2021/cse499/code_lab01/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.ece.nsu.spring2021.cse499.code_lab01
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}

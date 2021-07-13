861fdf7c3d1ef665ed547f581cfe82c8592c7a01
Rifat Islam <rifat.islam173@northsouth.edu>
2021-07-13 20:03:25 +0600

    Enhancement #16 : Project space created for UI design
    Further project related works will be done in this project space.

---
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ece.nsu.spring2021.cse499.arschoolbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

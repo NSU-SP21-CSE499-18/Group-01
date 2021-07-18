34ea865aee46bca55ddd018415207fb32ef31b6f
ferdouszislam <ferdouszislam@gmail.com>
2021-07-19 02:53:55 +0600

    Enhancement #18: ContentActivity.kt update
    Code inside ContentActivity.kt tidied up a bit.

--- Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/ContentActivity.kt
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/ContentActivity.kt
@@ -2,26 +2,53 @@ package com.ece.nsu.spring2021.cse499.arschoolbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 
+/**
+ * Activity Class for a single chapter on book
+ */
 class ContentActivity : AppCompatActivity() {
+
+    // ui
+    lateinit var backButton : ImageButton
+    lateinit var chapterNoTv: TextView
+    lateinit var chapterNameTv: TextView
+
+    // models
+    var chapterNo: String = ""
+    var chapterName: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
 
-        val backButton : ImageButton = findViewById(R.id.backBtn)
-        val chapterNoTv: TextView = findViewById(R.id.selected_ch_no)
-        val chapterNameTv: TextView = findViewById(R.id.selected_ch_name)
-        val bundle: Bundle? = intent.extras
-        val chapterNo: String = bundle?.get("Chapter-No").toString()
-        val chapterName : String = bundle?.get("Chapter-Name").toString()
+        init()
+    }
 
+    private fun init() {
+
+        // initialize UI components
+        backButton = findViewById(R.id.backBtn)
+        chapterNoTv = findViewById(R.id.selected_ch_no)
+        chapterNameTv = findViewById(R.id.selected_ch_name)
+
+        // get data for the UI
+        val bundle: Bundle? = intent.extras
+        chapterNo = bundle?.get("Chapter-No").toString()
+        chapterName = bundle?.get("Chapter-Name").toString()
+
+        // set data to UI
         chapterNoTv.text = chapterNo
         chapterNameTv.text = chapterName
+    }
 
-        backButton.setOnClickListener { finish() }
-
+    /**
+     * back button press listener
+     */
+    fun backClick(view: View) {
+        finish()
     }
 }

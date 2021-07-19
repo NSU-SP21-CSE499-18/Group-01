f1a1dd8587c3083e4465f55635a9d70813a2bd1a
ferdouszislam <ferdouszislam@gmail.com>
2021-07-19 18:46:01 +0600

    Enhancement #18: YouTubeVideo.kt create
    YouTubeVideo.kt model class for holding information of a single youtube video from database.

---
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/models/YouTubeVideo.kt
@@ -0,0 +1,6 @@
+package com.ece.nsu.spring2021.cse499.arschoolbook.models
+
+/**
+ * model class for a single YouTube video
+ */
+data class YouTubeVideo(var link: String? = "", var source: String? = "", var videoId: String? = "")

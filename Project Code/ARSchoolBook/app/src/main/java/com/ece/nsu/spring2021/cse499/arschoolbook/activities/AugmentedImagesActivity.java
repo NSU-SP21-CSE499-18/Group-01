071068328f9ac087459fc059eda7edb12bbb35a9
ferdouszislam <ferdouszislam@gmail.com>
2021-08-02 23:55:34 +0600

    Enhancement #20: AR fragment added to AugmentedImagesActivity
    AR fragment added to activity_augmented_images.xml and initialized in AugmentedImagesActivity

--- Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/activities/AugmentedImagesActivity.java
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/activities/AugmentedImagesActivity.java
@@ -1,16 +1,60 @@
 package com.ece.nsu.spring2021.cse499.arschoolbook.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentOnAttachListener;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ece.nsu.spring2021.cse499.arschoolbook.R;
+import com.google.ar.core.Config;
+import com.google.ar.core.Session;
+import com.google.ar.sceneform.Sceneform;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
 
-public class AugmentedImagesActivity extends AppCompatActivity {
+public class AugmentedImagesActivity extends AppCompatActivity implements
+        FragmentOnAttachListener, BaseArFragment.OnSessionConfigurationListener {
+
+    // ui
+    private ArFragment arFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_augmented_images);
+
+        init(savedInstanceState);
+    }
+
+    private void init(Bundle savedInstanceState) {
+
+        getSupportFragmentManager().addFragmentOnAttachListener(this);
+
+        if (savedInstanceState == null) {
+            if (Sceneform.isSupported(this)) {
+                // setup the ARFragment
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.arFragment, ArFragment.class, null)
+                        .commit();
+            }
+            else Toast.makeText(this, "Your device does not support AR", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull FragmentManager fragmentManager, @NonNull Fragment fragment) {
+        if (fragment.getId() == R.id.arFragment) {
+            this.arFragment = (ArFragment) fragment;
+            this.arFragment.setOnSessionConfigurationListener(this);
+        }
+    }
+
+    @Override
+    public void onSessionConfiguration(Session session, Config config) {
+
     }
 }

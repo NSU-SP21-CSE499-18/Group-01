9083a310d3fd959d0b29a0f99ee059ab0553bbab
ferdouszislam <ferdouszislam@gmail.com>
2021-08-02 23:34:12 +0600

    Enhancement #20: Create AugmentedImagesActivity

---
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/activities/AugmentedImagesActivity.java
@@ -0,0 +1,16 @@
+package com.ece.nsu.spring2021.cse499.arschoolbook.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ece.nsu.spring2021.cse499.arschoolbook.R;
+
+public class AugmentedImagesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_augmented_images);
+    }
+}

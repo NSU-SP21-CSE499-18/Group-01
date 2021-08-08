3c13363a53724da9a0da0d2e6e652ed2a27e8a81
ferdouszislam <ferdouszislam@gmail.com>
2021-08-08 06:24:42 +0600

    Enhancement #20: Merge conflict fixes, Updated 3D models added
    - Remaining merge conflict issues with main and issue20-AR_Figures branch found at compile time fixed.
    
    - Updated 3D models added to AS project.

--- Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/activities/ContentActivity.kt
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/activities/ContentActivity.kt
@@ -2,7 +2,6 @@ package com.ece.nsu.spring2021.cse499.arschoolbook.activities
 
 import android.content.Intent
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.speech.RecognizerIntent

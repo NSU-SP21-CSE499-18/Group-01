70afac2faad00696ebc79e86920bfe92551367c8
Rifat Islam <rifat.islam173@northsouth.edu>
2021-08-14 19:08:27 +0600

    Enhancement #27: Added new activity (SelectFigureActivity)

---
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/activities/SelectFigureActivity.kt
@@ -0,0 +1,12 @@
+package com.ece.nsu.spring2021.cse499.arschoolbook.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ece.nsu.spring2021.cse499.arschoolbook.R
+
+class SelectFigureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_figure)
+    }
+}

1e5996dcc9b6ba109e077d65176b2abb6d2445a2
Rifat Islam <rifat.islam173@northsouth.edu>
2021-09-08 18:30:15 +0600

    Enhancement #32: New home activity added

---
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/GeHomeActivity.kt
@@ -0,0 +1,11 @@
+package com.ece.nsu.spring2021.cse499.arschoolbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeHomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ge_home)
+    }
+}
--- Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/adpters/SelectClassAdapter.kt
+++ Project Code/ARSchoolBook/app/src/main/java/com/ece/nsu/spring2021/cse499/arschoolbook/adpters/SelectClassAdapter.kt
@@ -1,16 +1,20 @@
 package com.ece.nsu.spring2021.cse499.arschoolbook.adpters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.ece.nsu.spring2021.cse499.arschoolbook.R
+import com.ece.nsu.spring2021.cse499.arschoolbook.activities.SelectFigureActivity
 
 //Add callback function
 class SelectClassAdapter(private val mList: Array<String>, private val context: Context) : RecyclerView.Adapter<SelectClassAdapter.ViewHolder>() {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -23,16 +27,20 @@ class SelectClassAdapter(private val mList: Array<String>, private val context: 
         holder.textView.text = mList[position]
 
         //Replace it with callbacks
-        holder.textView.setOnClickListener {
+        holder.layout.setOnClickListener {
 
-
-            Toast.makeText(context, holder.textView.text, Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, SelectFigureActivity::class.java)
+            intent.putExtra("SelectedClass", holder.textView.text.toString().toLowerCase())
+            context.startActivity(intent)
+            Toast.makeText(context, holder.textView.text.toString().toLowerCase(), Toast.LENGTH_SHORT).show()
         }
     }
+
 
     override fun getItemCount(): Int {return mList.size }
 
     class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
         val textView: TextView = itemView.findViewById(R.id.className)
+        val layout: LinearLayout = itemView.findViewById(R.id.class_layout)
     }
 }
